7481de21bdfdd4cbdffc53eae03d6dc4059e32d2
Daniell-0211 <cty0211@gmail.com>
2021-03-29 10:55:10 +0800

    Initial commit

---
+++ 1502_Fragment_BMI/src/main/java/tw/tcnr05/m1502/M1502.kt
@@ -0,0 +1,40 @@
+package tw.tcnr05.m1502
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import tw.tcnr05.m1502.BMIFragment.BMIListener
+
+class M1502 : AppCompatActivity(), BMIListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.m1502)
+    }
+
+    // 實作BMIListener介面方法
+    override fun onButtonClick(bmi: Double) {
+        val fm = supportFragmentManager
+        val tf = fm.findFragmentById(R.id.fragment2) as TextFragment?
+        //  呼叫TextFragment物件的方法
+        tf!!.changeBMIValue(bmi)
+    }
+
+    //=====================================================
+    override fun onBackPressed() {
+//        super.onBackPressed();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.m_return, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_settings->finish()
+        }
+        //finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
